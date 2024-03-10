da2977ae428c9c001e61cbbf58f5cda5a8d41e5b
Jonathas Xavier <102988454+XavJon11@users.noreply.github.com>
2024-03-10 13:12:36 -0300

    Add files via upload

---
+++ Target Av/3quest.js
@@ -0,0 +1,25 @@
+//3quest
+/*a) 1, 3, 5, 7, ___
+
+O próximo número será 7 + 2 = 9.
+b) 2, 4, 8, 16, 32, 64, ____
+
+
+O próximo número será 64 * 2 = 128.
+c) 0, 1, 4, 9, 16, 25, 36, ____
+
+
+O próximo número será 7^2 = 49.
+d) 4, 16, 36, 64, ____
+
+
+O próximo número será 10^2 = 100.
+e) 1, 1, 2, 3, 5, 8, ____
+
+
+O próximo número será a soma dos dois números anteriores: 5 + 8 = 13.
+f) 2,10, 12, 16, 17, 18, 19, ____
+
+
+Uma possível lógica seria adicionar 8 ao número anterior até chegar em 19.
+O próximo número seria 19 + 8 = 27.*/
---
+++ Target Av/5quest.js
@@ -0,0 +1,19 @@
+//5quest
+
+function inverterString(str) {
+    let invertedString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        invertedString += str[i];
+    }
+    return invertedString;
+}
+
+// String a ser invertida
+let minhaString = "Hello, world!";
+
+// Chamada da função para inverter a string
+let stringInvertida = inverterString(minhaString);
+
+// Exibir a string invertida
+console.log("String original:", minhaString);
+console.log("String invertida:", stringInvertida);
